22fcf5ae2d8c7caee982681b7067682b5cf7d1b2
jansonfern <127862089+jansonfern@users.noreply.github.com>
2024-10-16 22:11:13 -0300

    Update thread2.rb

--- thread2.rb
+++ thread2.rb
@@ -1,10 +1,15 @@
 def tarefa_multi_thread
+  threads = []
   10.times do |i|
-    Thread.new do
+    thread = Thread.new do
       sleep(0.1)
       puts "Tarefa #{i}"
+      $stdout.flush
     end
+    threads << thread
   end
+
+  threads.each(&:join)
 end
 
 tarefa_multi_thread
