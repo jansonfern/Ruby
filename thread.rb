6fc954b5b55f3d173542efef8567f3bbae339700
jansonfern <127862089+jansonfern@users.noreply.github.com>
2024-10-16 22:18:31 -0300

    Update thread.rb

--- thread.rb
+++ thread.rb
@@ -1,26 +1,44 @@
 class MinhaTarefa
-  attr_reader :id
+  attr_reader :id, :data
 
-  def initialize(id)
+  def initialize(id, data)
     @id = id
+    @data = data
   end
 
-  def run
+  def call
     sleep(0.1)
-    puts "Tarefa #{@id}"  
+    puts "Tarefa #{id} com dados: #{@data}"
   end
 end
 
-def tarefa_com_classe
-  threads = []
+# Executa uma única tarefa
+def tarefa_unica
+  MinhaTarefa.new(1, 'Dados da tarefa única').call
+end
+
+# Executa 10 tarefas em threads separadas
+def tarefa_multi_thread_simples
   10.times do |i|
-    thread = Thread.new(MinhaTarefa.new(i)) do |tarefa|
-      tarefa.run  
-    end
-    threads << thread
+    Thread.new { sleep(0.1); puts "Tarefa #{i}" }
   end
-
-  threads.each(&:join)
 end
 
-tarefa_com_classe
+# Executa 10 tarefas em threads separadas, utilizando a classe MinhaTarefa
+def tarefa_multi_thread_com_classe
+  threads = 10.times.map do |i|
+    Thread.new(MinhaTarefa.new(i, "Dados da tarefa #{i}")) do |tarefa|
+      begin
+        tarefa.call
+      rescue StandardError => e
+        puts "Erro na tarefa #{i}: #{e.message}"
+      end
+    end
+  end
+  threads.each(&:join) 
+end
+
+
+tarefa_unica
+tarefa_multi_thread_simples
+tarefa_multi_thread_com_classe
