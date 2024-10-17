5ec4c58f25d0506c5cf519d7e8ac6035d3ae9da0
jansonfern <127862089+jansonfern@users.noreply.github.com>
2024-10-16 22:13:16 -0300

    Update thread3.rb

--- thread3.rb
+++ thread3.rb
@@ -1,22 +1,27 @@
 class MinhaTarefa
-  attr_reader :id
+  attr_reader :id, :data
 
-  def initialize(id)
+  def initialize(id, data)
     @id = id
+    @data = data
   end
 
   def run
     sleep(0.1)
-    puts "Tarefa #{id}"
+    puts "Tarefa #{id} com dados: #{@data}"
   end
 end
 
 def tarefa_com_classe
+  threads = []
   10.times do |i|
-    Thread.new(MinhaTarefa.new(i)) do |tarefa|
+    thread = Thread.new(MinhaTarefa.new(i, "Algum dado #{i}")) do |tarefa|
       tarefa.run
     end
+    threads << thread
   end
+
+  threads.each(&:join)
 end
 
 tarefa_com_classe
