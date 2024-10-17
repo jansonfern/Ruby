cd561d8936d068b8e43c5e609d39dbaa144391e7
jansonfern <127862089+jansonfern@users.noreply.github.com>
2024-10-16 22:09:54 -0300

    Update thread.rb

--- thread.rb
+++ thread.rb
@@ -1,8 +1,26 @@
-def tarefa_sequencial
-  10.times do |i|
-    sleep(0.1) # Simulando uma tarefa demorada
-    puts "Tarefa #{i}"
+class MinhaTarefa
+  attr_reader :id
+
+  def initialize(id)
+    @id = id
+  end
+
+  def run
+    sleep(0.1)
+    puts "Tarefa #{@id}"  
   end
 end
 
-tarefa_sequencial
+def tarefa_com_classe
+  threads = []
+  10.times do |i|
+    thread = Thread.new(MinhaTarefa.new(i)) do |tarefa|
+      tarefa.run  
+    end
+    threads << thread
+  end
+
+  threads.each(&:join)
+end
+
+tarefa_com_classe
